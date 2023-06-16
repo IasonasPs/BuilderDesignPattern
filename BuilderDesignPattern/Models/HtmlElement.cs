e8ac1271da96df056d7db4ada9386d15e37f948c
Iasonas <iasonas.ps@gmail.com>
2023-06-16 15:35:27 +0300

    Add project files.

---
+++ BuilderDesignPattern/Models/HtmlElement.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace BuilderDesignPattern.Models
+{
+    public class HtmlElement
+    {
+        private const int indentSize = 2;
+        public List<HtmlElement> Elements = new List<HtmlElement>(); //List of child elements
+        public string Name;
+        public string Text;
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', indentSize * indent);
+            sb.AppendLine($"{i}<{Name}>");
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', indentSize * ( indent + 1 )));
+                sb.AppendLine(Text);
+            }
+            foreach (var element in Elements)
+            {
+                sb.Append(element.ToStringImpl(indent + 3));  //recursive
+            }
+            sb.AppendLine($"{i}</{Name}>");
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ BuilderDesignPattern/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Text;
+using BuilderDesignPattern.Models;
+using static System.Console;
+
+namespace BuilderDesignPattern
+{
+    public class HtmlElement : Models.HtmlElement
+    {
+        public HtmlElement()
+        {
+
+        }
+        public HtmlElement(string name, string text)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: "name");
+            Text = text ?? throw new ArgumentNullException(paramName: "text");
+        }
+        public HtmlElement(string name, string text, List<HtmlElement> e)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: "name");
+            Text = text ?? throw new ArgumentNullException(paramName: "text");
+            Elements = e.ToList();
+        }
+    }
+
+    public class HtmlBuilder
+    {
+        private readonly string rootName;
+        HtmlElement root = new HtmlElement();
+        public HtmlBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public void AddChild(string childName,string childText)
+        {
+            var e = new HtmlElement(childName,childText);
+            root.Elements.Add(e);
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+        public void Clear()
+        {
+            root =  new HtmlElement {Name =  rootName};
+            root.Elements.Clear();
+        }
+    }
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Introduction
+            //var hello = "hello";
+            //var sb = new StringBuilder();
+            //sb.Append("<p>");
+            //sb.Append(hello);
+            //sb.Append("</p>");
+            //WriteLine(sb);
+
+            //var words = new[] { "What", "a", "beatiful", "World!!" };
+            //sb.Clear();
+            //sb.Append("<ul>");
+            //foreach (var word in words)
+            //{
+            //    sb.AppendFormat("<li>{0}</li>", word);
+            //}
+            //sb.Append("</ul>");
+            //WriteLine(sb); 
+            #endregion
+
+            //var list = new List<HtmlElement>();
+            //HtmlElement h0 = new HtmlElement("h0", "thats ");
+            //list.Add(h0);
+            //HtmlElement h1 = new HtmlElement("h1", "thats ");
+            //list.Add(h1);
+            //HtmlElement h2 = new HtmlElement("h2", "thats an encapsulated text", list);
+            //list.Add(h2);
+            //HtmlElement he3 = new HtmlElement("h3", "thats an encapsulated text", list);
+            //list.Add(he3);
+            //HtmlElement h = new HtmlElement("h", "thats a text", list);
+
+            //WriteLine(h.ToString());
+
+            var b = new HtmlBuilder("ul");
+            //b.AddChild("h1","Welcome");
+
+
+
+            WriteLine(b);
+        }
+    }
+}
